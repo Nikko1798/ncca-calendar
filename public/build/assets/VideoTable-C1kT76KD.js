938ca4b41e5ccaf855180a59f140ea2daecfee7d
Nikko Niel Zamora <nikkoniel1@gmail.com>
2026-06-15 23:03:43 +0000

    Build assets for production

---
+++ public/build/assets/VideoTable-C1kT76KD.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./VideoTable.vue_vue_type_script_setup_true_lang-YxzXCytc.js";import"./utils-DjqsqOe8.js";import"./app-DY65w9Rl.js";export{o as default};
--- public/build/assets/VideoTable-CdOkDTzO.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./VideoTable.vue_vue_type_script_setup_true_lang-D9t9erz1.js";import"./utils-DjqsqOe8.js";import"./app-C7qftLuf.js";export{o as default};
